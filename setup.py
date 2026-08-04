e0f84f766a111767057741685457d280d5255f1c
Adhi262004 <aadhikumar262004@gmail.com>
2026-08-04 10:02:54 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+name='cnnClassifier',
+version='0.0.0',
+author='Adhi262004',
+author_email='aadhikumar262004@gmail.com',
+packages=find_packages(),
+install_requires=[]
+)
