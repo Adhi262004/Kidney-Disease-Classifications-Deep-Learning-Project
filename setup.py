da8a7af79cc44e373c95be6c0b4391d9241ecd5a
Adhi262004 <aadhikumar262004@gmail.com>
2026-08-05 19:46:09 +0530

    data ingestion added

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 
 setup(
-name='cnnClassifier',
-version='0.0.0',
-author='Adhi262004',
-author_email='aadhikumar262004@gmail.com',
-packages=find_packages(),
-install_requires=[]
+    name='cnnClassifier',
+    version='0.0.0',
+    author='Aadhi262004',
+    author_email='aadhikumar262004@gmail.com',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[]
 )
